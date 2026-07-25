2d44f98b1d3417165ed2c03e56e8e0eeaa772f55
Swati Garewal <swatigarewal35@gmail.com>
2026-07-25 14:51:15 +0530

    updated

--- project001/project001/settings.py
+++ project001/project001/settings.py
@@ -139,8 +139,8 @@ LOGIN_URL = '/accounts/login/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
@@ -148,11 +148,9 @@ EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-print("EMAIL_HOST_USER:", EMAIL_HOST_USER)
-print("EMAIL_HOST_PASSWORD FOUND:", bool(EMAIL_HOST_PASSWORD))
-EMAIL_FAIL_SILENTLY = False
-EMAIL_TIMEOUT = 10
+DEFAULT_FROM_EMAIL = f"LearnCodePlay <{EMAIL_HOST_USER}>"
+
+EMAIL_TIMEOUT = 30
 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
