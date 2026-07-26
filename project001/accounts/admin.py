46145ef269f950c091416d0a8f452272f73a9925
Swati Garewal <swatigarewal35@gmail.com>
2026-07-26 18:19:11 +0530

    complete project

--- project001/accounts/admin.py
+++ project001/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GameComment
+from .models import *
 
 
 @admin.register(GameComment)
@@ -7,3 +7,22 @@ class GameCommentAdmin(admin.ModelAdmin):
     list_display = ('game_id', 'user', 'comment_text', 'created_at')
     list_filter = ('game_id', 'created_at')
     search_fields = ('comment_text', 'game_id')
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'image',
+        'user',
+        'get_email',
+        'mobile',
+        'gender',
+        'dob',
+        'profession',
+        
+    )
+    search_fields = ('user__username', 'user__email', 'mobile')
+    list_filter = ('gender', 'profession')
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
